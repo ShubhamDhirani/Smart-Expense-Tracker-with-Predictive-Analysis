5dcc8c9d9fec4f368689fe48f29a3bb9ad7874ff
ShubhamDhirani <shubhamdhirani@gmail.com>
2026-01-04 21:07:08 +0530

    feat: add database session dependency

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 DATABASE_URL = "sqlite:///./expenses_tracker.db"
 
@@ -15,3 +15,11 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+
+    finally:
+        db.close()  
