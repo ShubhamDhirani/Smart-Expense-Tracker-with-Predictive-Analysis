0c75a096db6d693f661754deeb82a9c1349a858e
ShubhamDhirani <shubhamdhirani@gmail.com>
2026-01-10 15:06:24 +0530

    fix: enable CORS for frontend integration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import engine
 from .models import Base
-from .routes import auth, expenses, analytics
+from .routes import auth, expenses, analytics, prediction
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials = True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(expenses.router)
 app.include_router(analytics.router)
+app.include_router(prediction.router)
 
 @app.get("/")
 def root():
