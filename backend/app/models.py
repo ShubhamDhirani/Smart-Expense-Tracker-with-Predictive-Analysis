a5560225eb5804ecb5aabd862cc0fb0c1bd201fc
ShubhamDhirani <shubhamdhirani@gmail.com>
2026-03-23 09:58:54 +0530

    Added Category model and updated Expense schema for structured categories and recurring support

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,21 @@
-from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey, Boolean, DateTime
 from sqlalchemy.orm import relationship
 from .database import Base
+from datetime import datetime
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True,index = True)
+    name = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+    frequency = Column(String, nullable=True)
+    is_global = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("users.id"),nullable=True)
+
+    created_at = Column(DateTime, default= datetime.utcnow)
+    user= relationship("User")
+
 class User(Base):
     __tablename__ = "users"
 
@@ -17,7 +32,10 @@ class Expense(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     amount = Column(Float)
-    category = Column(String) 
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    is_recurring = Column(Boolean, default = False)
+    last_added_date = Column(Date, nullable = True)
+    category = relationship("Category") 
     payment_mode = Column(String) 
     date = Column(Date)
     description = Column(String)
